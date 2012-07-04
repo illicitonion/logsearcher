c5e2c22f690c1ce701d720544123f62d1bcd9c99
Daniel Wagner-Hall <dawagner@gmail.com>
2012-07-04 23:50:42 +0100

    Adding log file searcher

---
+++ src/logsearcher/file_searcher.go
@@ -0,0 +1,78 @@
+package logsearcher
+
+import (
+  "bufio"
+  "io"
+  "os"
+  "strings"
+)
+
+type Predicate func(string) bool
+
+type FileEntry struct {
+  path string
+  snippets chan string
+}
+
+func GetFileSnippets(dir string, predicate Predicate, files chan FileEntry) {
+  rawFiles := make(chan string)
+  go func() {
+    for {
+      filename,open := <- rawFiles
+      if !open {
+        close(files)
+        break;
+      }
+      
+      lines := make(chan string)
+      filteredLines := make(chan string)
+
+      go Filter(lines, predicate, filteredLines)
+      go ReadLines(dir + "/" + filename, lines)
+
+      files <- FileEntry{filename, filteredLines}
+    }
+  }()
+  ListFiles(dir, rawFiles)
+} 
+
+func ReadLines(path string, ch chan string) (err error) {
+  var file *os.File
+  file, err = os.Open(path)
+  if err != nil {
+    close(ch)
+    return
+  }
+  defer file.Close()
+
+  reader := bufio.NewReader(file)
+  for {
+    var line string
+    line, err = reader.ReadString('\n')
+    if err != nil && err != io.EOF {
+      return
+    }
+    trimmed := strings.Trim(line, "\n\r")
+    if len(trimmed) > 0 {
+      ch <- trimmed
+    }
+    if err == io.EOF {
+      close(ch)
+      return nil
+    }
+  }
+  return
+}
+      
+func Filter(in chan string, predicate Predicate, out chan string)  {
+  for {
+    e, open := <- in
+    if !open {
+      close(out)
+      break
+    }
+    if predicate(e) {
+      out <- e
+    }
+  }
+}
---
+++ src/logsearcher/file_searcher_test.go
@@ -0,0 +1,105 @@
+package logsearcher
+
+import (
+  "strconv"
+  "testing"
+)
+
+func TestReadsLines(t *testing.T) {
+  ch := make(chan string)
+  go assertChanReturns(t, []string{"1","2","3","4","5"}, ch)
+  err := ReadLines("../../testdata/files/2.txt", ch)
+  checkNoError(t, err)
+}
+
+func TestFiltersLines(t *testing.T) {
+  in := make(chan string)
+  out := make(chan string)
+  go func() {
+    in <- "1"
+    in <- "2"
+    in <- "3"
+    in <- "4"
+    in <- "5"
+    close(in)
+  }()
+  go assertChanReturns(t, []string{"2","4"}, out)
+
+  isEven := func(item string) bool {
+    num,_ := strconv.Atoi(item)
+    return num % 2 == 0
+  }
+
+  Filter(in, isEven, out)
+}
+
+func assertFileEntryEquals(t *testing.T, expected FileEntry, actual FileEntry) {
+  defer consume(expected.snippets)
+  defer consume(actual.snippets)
+
+  if expected.path != actual.path {
+    t.Fatalf("Incorrect path: Expected %v but was %v", expected.path, actual.path)
+  }
+  for {
+    expectedLine,expectedOk := <- expected.snippets
+    actualLine,actualOk := <- actual.snippets
+    if !expectedOk && !actualOk {
+      break
+    } else if expectedOk != actualOk {
+      t.Errorf("Different numbers of entries in expected and actual snippets")
+    } else if expectedOk && expectedLine != actualLine {
+      t.Errorf("Expected snippet %v but was %v", expectedLine, actualLine)
+    }
+  }
+}
+
+func consumeFiles(ch chan FileEntry) {
+  for {
+    _,more := <- ch
+    if !more {
+      break
+    }
+  }
+}
+
+func stubChannel(values []string) (ch chan string) {
+  ch = make(chan string)
+  go func() {
+    for i := range values {
+      ch <- values[i]
+    }
+    close(ch)
+  }()
+  return
+} 
+
+func TestGetsFileSnippets(t *testing.T) {
+  files := make(chan FileEntry)
+
+  go GetFileSnippets("../../testdata/files", func(_ string) bool {
+    return true
+  }, files)
+
+  expectedOne := FileEntry{"1.txt", stubChannel([]string{"1"})}
+  expectedTwo := FileEntry{"2.txt", stubChannel([]string{"1","2","3","4","5"})}
+
+  var file FileEntry
+  var ok bool
+
+  file,ok = <- files
+  if !ok {
+    t.Fatal("Expected file")
+  }
+  assertFileEntryEquals(t, expectedOne, file)
+
+  file,ok = <- files
+  if !ok {
+    t.Fatal("Expected file")
+  }
+  assertFileEntryEquals(t, expectedTwo, file)
+
+  file,ok = <- files
+  if ok {
+    t.Fatalf("Expected no more files but found %v", file.path)
+  }
+}
