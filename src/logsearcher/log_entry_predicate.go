21593f4a69764a25f01de935915f51e47d66254a
Daniel Wagner-Hall <dawagner@gmail.com>
2012-07-05 00:25:31 +0100

    Adding log entry predicate

---
+++ src/logsearcher/log_entry_predicate.go
@@ -0,0 +1,19 @@
+package logsearcher
+
+import(
+  "strings"
+)
+
+func logEntryPredicate(usernameMixedCase string, messageMixedCase string) Predicate {
+  username := strings.ToLower(usernameMixedCase)
+  message := strings.ToLower(messageMixedCase)
+
+  return func(strMixedCase string) bool {
+    str := strings.ToLower(strMixedCase)
+    fields := strings.Fields(str)
+
+    return len(fields) > 1 &&
+      (len(username) == 0 || username + ":" == fields[1]) &&
+      strings.Contains(str, message)
+  }
+}
---
+++ src/logsearcher/log_entry_predicate_test.go
@@ -0,0 +1,70 @@
+package logsearcher
+
+import (
+  "testing"
+)
+
+func assertMatches(t *testing.T, predicate Predicate, s string, expected bool) {
+  if predicate(s) != expected {
+    t.Errorf("Expected predicate to be %v for [%v]\n", expected, s)
+  }
+}
+
+func TestNameAndMessage(t *testing.T) {
+  predicate := logEntryPredicate("dawagner", "message")
+  truths := []string{
+    "[23:15:33] dawagner: some message",
+    "[00:00:00] dawagner: Some other some message",
+  }
+  for i := range truths {
+    assertMatches(t, predicate, truths[i], true)
+  }
+  falsehoods := []string{
+    "[23:15:33] simonstewart: some message",
+    "[00:00:00] dawagner: Something else",
+  }
+  for i := range falsehoods {
+    assertMatches(t, predicate, falsehoods[i], false)
+  }
+}
+
+func TestJustName(t *testing.T) {
+  predicate := logEntryPredicate("dawagner", "")
+  truths := []string{
+    "[23:15:33] dawagner: something",
+    "[00:00:00] dawagner:",
+  }
+  for i := range truths {
+    assertMatches(t, predicate, truths[i], true)
+  }
+  falsehoods := []string{
+    "[23:15:33] simonstewart: something",
+    "[00:00:00] PenguinOfDeath:",
+  }
+  for i := range falsehoods {
+    assertMatches(t, predicate, falsehoods[i], false)
+  }
+}
+
+func TestJustMessage(t *testing.T) {
+  predicate := logEntryPredicate("", "some message")
+  truths := []string{
+    "[23:15:33] dawagner: some message with trailing",
+    "[23:15:34] dawagner: some message",
+    "[00:00:00] simonstewart: leading some message",
+  }
+  for i := range truths {
+    assertMatches(t, predicate, truths[i], true)
+  }
+  falsehoods := []string{
+    "[23:15:33] dawagner: somemessage with trailing",
+    "[00:00:00] simonstewart: leading somemessage",
+  }
+  for i := range falsehoods {
+    assertMatches(t, predicate, falsehoods[i], false)
+  }
+}
+
+func TestCaseInsensitivity(t *testing.T) {
+  assertMatches(t, logEntryPredicate("daWagner", "some Message"), "[23:15:33] Dawagner: Some message with trailing", true)
+}
