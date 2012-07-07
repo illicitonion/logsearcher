e7892912578f8077e80a3e236cd2e73911fdd88d
Daniel Wagner-Hall <dawagner@gmail.com>
2012-07-07 15:44:08 +0100

    Pulling out some methods, tidying up web server

--- src/logsearcher/web_server.go
+++ src/logsearcher/web_server.go
@@ -35,87 +35,64 @@ const formBody string = `<p>Both fields are optional, but it would be really nic
     <input type="submit" value="Search" />
   </div>`
 
+func (server LogSearchServer) writeFolders(w http.ResponseWriter, folders chan string, closed *bool) {
+  for {
+    folder,ok := <- folders
+    if !ok {
+      *closed = true
+      return
+    }
+    fmt.Fprintf(w, "<p><a href='%v%v'>%v</a></p>", server.LogHttpRoot, folder, folder)
+  }
+}
+
+func (server LogSearchServer) writeContent(w http.ResponseWriter, files chan FileEntry, closed *bool) {
+  for {
+    file,ok := <- files
+    if !ok {
+      break
+    }
+    printedAny := false
+    for {
+      snippet,ok := <- file.Snippets
+      if !ok{
+        if printedAny {
+          fmt.Fprint(w, "</ul>")
+        }
+        break
+      }
+      if !printedAny {
+        fmt.Fprintf(w, "<h3><a href='%v%v'>%v</a></h3><ul>", server.LogHttpRoot, file.Path, file.Path)
+        printedAny = true
+      }
+      fmt.Fprintf(w, "<li>%v</li>", server.Escape(snippet))
+    }
+  }
+  *closed = true
+}
+
 func (server LogSearchServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   w.Header()["Connection"] = []string{"close"}
-  _,printerr := fmt.Fprint(w, header)
-  if printerr != nil {
-    return
-  }
+  fmt.Fprint(w, header)
 
   closed := false
   queryValues,_ := url.ParseQuery(r.URL.RawQuery)
   usernames,uok := queryValues["username"]
   messages,mok := queryValues["message"]
   if !uok || !mok {
-    _,printerr := fmt.Fprint(w, formBody)
-    if printerr != nil {
-      return
-    }
+    fmt.Fprint(w, formBody)
     folders := make(chan string)
-    go func() {
-      for {
-        folder,ok := <- folders
-        if !ok {
-          _,printerr := fmt.Fprint(w, footer)
-          closed = true
-          if printerr != nil {
-            return
-          }
-          break
-        }
-        _,printerr := fmt.Fprintf(w, "<p><a href='%v%v'>%v</a></p>", server.LogHttpRoot, folder, folder)
-        if printerr != nil {
-          return
-        }
-      }
-    }()
+    go server.writeFolders(w, folders, &closed)
     ListFolders(server.LogPath, folders)
-    for !closed {
-      runtime.Gosched()
-    }
-    return
+  } else {
+    files := make(chan FileEntry)
+    go server.writeContent(w, files, &closed)
+    GetFileSnippets(server.LogPath, LogEntryPredicate(usernames[0], messages[0]), files)
   }
 
-  username := usernames[0]
-  message := messages[0]
-
-  files := make(chan FileEntry)
-  go func() {
-    for {
-      file,ok := <- files
-      if !ok {
-        break
-      }
-      printed := false
-      for {
-        snippet,err := <- file.Snippets
-        if !err {
-          _,printerr := fmt.Fprint(w, "</ul>")
-          if printerr != nil {
-            return
-          }
-          break
-        }
-        if !printed {
-          _,printerr := fmt.Fprintf(w, "<h3><a href='%v%v'>%v</a></h3><ul>", server.LogHttpRoot, file.Path, file.Path)
-          if printerr != nil {
-            return;
-          }
-          printed = true
-        }
-        _,printerr := fmt.Fprintf(w, "<li>%v</li>", server.Escape(snippet))
-        if printerr != nil {
-          return
-        }
-      }
-    }
-    fmt.Fprint(w, footer)
-    closed = true
-  }()
-
-  GetFileSnippets(server.LogPath, LogEntryPredicate(username, message), files)
   for !closed {
     runtime.Gosched()
   }
+  fmt.Fprint(w, footer)
 }
 
